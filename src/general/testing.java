b7602872185ea274a18fb2f0b89ae50f4ba42010
xeij <q08d@aol.com>
2024-03-26 23:44:41 -0700

    3/26/2024

---
+++ src/general/testing.java
@@ -0,0 +1,14 @@
+package general;
+
+import java.util.Scanner;
+
+public class testing {
+    
+
+    public static void main(String[] args) throws Exception {   
+
+        Scanner scanner = new Scanner(null)
+
+}
+
+}
