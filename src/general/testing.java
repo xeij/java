acf0a19150965832d50043a8331c9ef111747ea7
xeij <q08d@aol.com>
2024-03-27 18:54:04 -0700

    3/27/2024

--- src/general/testing.java
+++ src/general/testing.java
@@ -1,14 +1,18 @@
 package general;
 
 import java.util.Scanner;
+import java.io.*;
 
 public class testing {
-    
 
     public static void main(String[] args) throws Exception {   
 
-        Scanner scanner = new Scanner(null)
+        Scanner scanner = new Scanner(System.in);
+        File file = new File("musicPlayList.csv");
 
-}
+            System.out.println("\nFile Length: " + file.length() + "\n");
+            System.out.println("Absolute Path: " + file.getAbsolutePath()+ "\n");
+            scanner.close();
+                }
 
-}
+    }
