6133dfbd36468a5049fa44e81e85dbfee8d165c2
xeij <q08d@aol.com>
2024-04-07 00:06:51 -0700

    4/7/2024

---
+++ src/datas/bst.java
@@ -0,0 +1,43 @@
+package datas;
+
+public class bst {    
+    public static void main(String[] args) {    
+        bst bt = new bst();    
+        System.out.println("Total number of possible Binary Search Trees with given key: " + bt.numOfBST(5));    
+      }    
+    
+    public static class Node{    
+        int data;    
+        Node left;    
+        Node right;    
+        public Node(int data){    
+
+            this.data = data;    
+            this.left = null;    
+            this.right = null;    
+            }    
+        }    
+
+    public Node root;    
+    public bst(){    
+        root = null;    
+    }    
+
+    public int factorial(int num) {    
+        int fact = 1;    
+        if(num == 0)    
+            return 1;    
+        else {    
+            while(num > 1) {    
+                fact = fact * num;    
+                num--;    
+            }    
+            return fact;    
+        }    
+    }    
+
+    public int numOfBST(int key) {    
+        int catalanNumber = factorial(2 * key)/(factorial(key + 1) * factorial(key));    
+        return catalanNumber;    
+    }    
+}    
