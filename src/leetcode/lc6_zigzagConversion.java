4b724755075204381f0a59efb3e80eaf1d8b866a
xeij <q08d@aol.com>
2024-04-06 22:39:19 -0700

    4/6/2024

---
+++ src/leetcode/lc6_zigzagConversion.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        int n = s.length();
+        int diff = 2 * (numRows - 1);
+        int diagonalDiff = diff;
+        int secondIndex;
+        int index;
+        for (int i = 0; i < numRows; i++) {
+            index = i;
+
+           
