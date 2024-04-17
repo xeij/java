1b26cfc1a73e1dbbcd78a752a4c208c3ac941f49
xeij <q08d@aol.com>
2024-04-16 21:22:05 -0700

    4/16/2024

---
+++ src/datas/avlRotations.java
@@ -0,0 +1,50 @@
+public class avlRotations {
+    
+    AVLTreeUpdateHeight(node) {
+        leftHeight = -1
+        if (node⇢left != null)
+           leftHeight = node⇢left⇢height
+        rightHeight = -1
+        if (node⇢right != null)
+           rightHeight = node⇢right⇢height
+        node⇢height = max(leftHeight, rightHeight) + 1
+     }
+     
+     
+     AVLTreeSetChild(parent, whichChild, child) {
+        if (whichChild != "left" && whichChild != "right")
+           return false
+     
+        if (whichChild == "left")
+           parent⇢left = child
+        else
+           parent⇢right = child
+        if (child != null)
+           child⇢parent = parent
+     
+        AVLTreeUpdateHeight(parent)
+        return true
+     }
+     
+     
+     AVLTreeReplaceChild(parent, currentChild, newChild) {
+        if (parent⇢left == currentChild)
+           return AVLTreeSetChild(parent, "left", newChild)
+        else if (parent⇢right == currentChild)
+           return AVLTreeSetChild(parent, "right", newChild)
+        return false
+     }
+     
+     
+     AVLTreeGetBalance(node) {
+        leftHeight = -1
+        if (node⇢left != null)
+           leftHeight = node⇢left⇢height
+        rightHeight = -1
+        if (node⇢right != null)
+           rightHeight = node⇢right⇢height
+        return leftHeight - rightHeight
+     }
+
+
+}
