f3f6ccef2137b878dcbc4f7cc1e8ded6b137048a
xeij <q08d@aol.com>
2024-04-16 21:27:17 -0700

    4/16/2024

--- src/datas/avlRotations.java
+++ src/datas/avlRotations.java
@@ -4,60 +4,60 @@ public class avlRotations {
     
     AVLTreeUpdateHeight(node) {
         leftHeight = -1
-        if (node⇢left != null)
-           leftHeight = node⇢left⇢height
+        if (node.left != null)
+           leftHeight = node.left.height;
         rightHeight = -1
-        if (node⇢right != null)
-           rightHeight = node⇢right⇢height
-        node⇢height = max(leftHeight, rightHeight) + 1
+        if (node.right != null)
+           rightHeight = node.right.height;
+        node.height = max(leftHeight, rightHeight) + 1
      }
      
      
      AVLTreeSetChild(parent, whichChild, child) {
         if (whichChild != "left" && whichChild != "right")
-           return false
+           return false;
      
         if (whichChild == "left")
-           parent⇢left = child
+           parent.left = child;
         else
-           parent⇢right = child
+           parent.right = child;
         if (child != null)
-           child⇢parent = parent
+           child.parent = parent;
      
         AVLTreeUpdateHeight(parent)
-        return true
+        return true;
      }
      
      
      AVLTreeReplaceChild(parent, currentChild, newChild) {
-        if (parent⇢left == currentChild)
-           return AVLTreeSetChild(parent, "left", newChild)
-        else if (parent⇢right == currentChild)
-           return AVLTreeSetChild(parent, "right", newChild)
-        return false
+        if (parent.left == currentChild)
+           return AVLTreeSetChild(parent, "left", newChild);
+        else if (parent.right == currentChild)
+           return AVLTreeSetChild(parent, "right", newChild);
+        return false;
      }
      
      
      AVLTreeGetBalance(node) {
         leftHeight = -1
-        if (node⇢left != null)
-           leftHeight = node⇢left⇢height
-        rightHeight = -1
-        if (node⇢right != null)
-           rightHeight = node⇢right⇢height
-        return leftHeight - rightHeight
+        if (node.left != null)
+           leftHeight = node.left.height;
+        rightHeight = -1;
+        if (node.right != null)
+           rightHeight = node.right.height;
+        return leftHeight - rightHeight;
      }
 
      AVLTreeRotateRight(tree, node) {
-        leftRightChild = node⇢left⇢right
-        if (node⇢parent != null)
-           AVLTreeReplaceChild(node⇢parent, node, node⇢left)
+        leftRightChild = node.left.right;
+        if (node.parent != null)
+           AVLTreeReplaceChild(node.parent, node, node.left);
         else { 
-           tree⇢root = node⇢left
-           tree⇢root⇢parent = null
+           tree.root = node.left;
+           tree.root.parent = null;
         }
-        AVLTreeSetChild(node⇢left, "right", node)
-        AVLTreeSetChild(node, "left", leftRightChild)
+        AVLTreeSetChild(node.left, "right", node);
+        AVLTreeSetChild(node, "left", leftRightChild);
      }
 
 
