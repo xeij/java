0abc3138736b9c5fa326a15cedfee03be0bd9272
xeij <q08d@aol.com>
2024-04-19 02:45:22 -0700

    4/19/2024

---
+++ src/datas/LinkedListReversal.java
@@ -0,0 +1,42 @@
+package datas;
+class Node {
+    
+    int val;
+    Node next;
+    
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class LinkedListReversal {
+    
+    public static Node reverseLinkedList(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        
+        while (current != null) {
+            next = current.next;  
+            current.next = prev;  
+            prev = current;  
+            current = next;
+        }
+        return prev;  
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+
+        head = reverseLinkedList(head);
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+    }
+}
