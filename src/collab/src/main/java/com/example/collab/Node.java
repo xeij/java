1a64fa4932f45389524ca594ad9bca9083492ca7
xeij <q08d@aol.com>
2024-04-28 22:52:10 -0700

    4/28/2024

---
+++ src/collab/src/main/java/com/example/collab/Node.java
@@ -0,0 +1,11 @@
+package com.example.collab;
+class Node {
+    int value, height;
+    Node left, right;
+	public int data;
+
+    Node(int value) {
+        this.value = value;
+        height = 1;
+    }
+}
