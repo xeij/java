5e748a0f7d5b439c7ec86441b24adfab826a1f88
xeij <q08d@aol.com>
2024-05-21 17:30:20 -0700

    5/21/2024

---
+++ src/practiceIt/Chapter7-Arrays/printBackwards.java
@@ -0,0 +1,7 @@
+public class printBackwards{
+    public void method(int[] data){
+    for(int i=data.length-1; i>=0; i--){
+        System.out.println("element [" + i + "] is " + data[i]);
+        }
+    }
+}
