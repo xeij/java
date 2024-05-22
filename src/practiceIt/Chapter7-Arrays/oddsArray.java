0f2eb3b4859f421bc21361403ba3e0403d7ad7ec
xeij <q08d@aol.com>
2024-05-21 17:23:24 -0700

    5/21/2024

---
+++ src/practiceIt/Chapter7-Arrays/oddsArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class oddsArray{
+
+    public static void main(String[] args){
+
+    int elm = 0;
+    int[] odds = new int[22];
+    for(int i=-6; i<=38; i++){
+    if(i%2!=0){
+        odds[elm]=i;
+        elm++;
+            }
+        }
+    }
+}
