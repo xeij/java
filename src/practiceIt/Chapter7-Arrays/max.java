2e25f04d45a7d6ed7fffaa58b321865845666577
xeij <q08d@aol.com>
2024-05-21 17:27:33 -0700

    5/21/2024

--- src/practiceIt/Chapter7-Arrays/max.java
+++ src/practiceIt/Chapter7-Arrays/max.java
@@ -1,5 +1,5 @@
 public class max {
-    public int max(int array[]){
+    public int maxMethod(int array[]){
         int temp = array[0];
         for(int i=0;i<array.length; i++){
             if(array[i] > temp){
