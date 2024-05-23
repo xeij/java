06ff20551c26269a1792729c7279686e5f586be1
xeij <q08d@aol.com>
2024-05-22 22:01:02 -0700

    5/22/2024

--- src/practiceIt/Chapter7-Arrays/countStrings.java
+++ src/practiceIt/Chapter7-Arrays/countStrings.java
@@ -1,4 +1,4 @@
-public int countStrings(Stringp[] array, String target){
+public int countStrings(String[] array, String target){
 	int answer = 0;
 	for(int i=0; i<array.length; i++){
 		if(array[i] == target){
