1cabdc8d520e6194f36298e394ec7f199b4fdf0b
xeij <q08d@aol.com>
2024-05-23 19:24:41 -0700

    5/23/2024

--- src/practiceIt/Chapter7-Arrays/mode.java
+++ src/practiceIt/Chapter7-Arrays/mode.java
@@ -1,8 +1,16 @@
-public int mode(int[] array){
-		
-
-
-
-
-
-}
+public static int mode(int[] array) {
+		int[] spareArray = new int[101];
+		for (int i = 0; i < array.length; i++) {
+	        spareArray[array[i]]++;
+	    }
+		int mode = 101;
+	    int count = 0;
+	    for (int i = 0; i < spareArray.length; i++) {
+	        if (spareArray[i] > count) {
+	            count = spareArray[i];
+	            mode = i;
+	        }
+	    }
+	    
+	    return mode;
+	}
