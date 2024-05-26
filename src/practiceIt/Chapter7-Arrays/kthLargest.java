f146ce494b9b29e3d9791763b9803fa0c76b3560
xeij <q08d@aol.com>
2024-05-26 00:02:28 -0700

    5/25/2024

--- src/practiceIt/Chapter7-Arrays/kthLargest.java
+++ src/practiceIt/Chapter7-Arrays/kthLargest.java
@@ -1,23 +1,19 @@
-public static int kthLargest(int k, int[] array){
-sort(array);
-int answer = array[array.length - k];
-return answer;
-}
-
-private static void sort(int[] array){
-	boolean swapped = true;
-	int j=0;
-	int temp;
-	while(swapped){
-		swapped = false;
-		j++;
-		for(int i=0; i < array.length-1; i++){
-				if(array[i] > array[i+1]){
-					temp = array[i];
-					array[i] = array[i+1];
-					array[i+1] = temp;
-					swapped = true;
-				}
-		}
+public int kthLargest(int k, int[] array){
+boolean swapped = true;
+int j=0;
+int temp;
+while(swapped){
+	swapped = false;
+	j++;
+	for(int i=0; i < array.length-j-1; i++){
+			if(array[i] > array[i+1]){
+				temp = array[i];
+				array[i] = array[i+1];
+				array[i+1] = temp;
+				swapped = true;
+			}
 	}
+}
+int answer = array[array.length - (k+1)];
+return answer;
 }
