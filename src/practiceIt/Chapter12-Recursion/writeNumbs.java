2d9c4af98949d7579f3403647fb01bf2665d08f6
xeij <q08d@aol.com>
2024-05-28 16:30:06 -0700

    5/28/2024

---
+++ src/practiceIt/Chapter12-Recursion/writeNumbs.java
@@ -0,0 +1,10 @@
+public static void writeNums(int n) {
+    if (n < 1) {
+        throw new IllegalArgumentException();
+    } else if (n == 1) {
+        System.out.print("1");
+    } else {
+        writeNums(n - 1);
+        System.out.print(", " + n);
+    }
+}
