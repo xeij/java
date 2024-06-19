7cac93be78537c69d7a3a1b4460b83db853f1fb0
xeij <q08d@aol.com>
2024-06-18 22:09:26 -0700

    6/18/2024

---
+++ src/practiceIt/Chapter3-Parameters/processName.java
@@ -0,0 +1,6 @@
+public static void processName(Scanner console) {
+   System.out.print("Please enter your full name: ");
+   String firstName = console.next();
+   String lastName = console.next();
+   System.out.print("Your name in reverse order is " + lastName + ", " + firstName);
+}
