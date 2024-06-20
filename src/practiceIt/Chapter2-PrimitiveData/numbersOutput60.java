b210130a91691eafecd286c4fa0046faaffb6105
xeij <q08d@aol.com>
2024-06-20 15:34:53 -0700

    6/20/2024

---
+++ src/practiceIt/Chapter2-PrimitiveData/numbersOutput60.java
@@ -0,0 +1,12 @@
+for (int i = 1; i <= 6; i++) {
+    for (int j = 1; j <= 9; j++) {
+        System.out.print(" ");
+    }
+    System.out.print("|");
+}
+
+System.out.println();
+
+for (int i = 1; i <= 6; i++) {
+    System.out.print("1234567890");
+}
