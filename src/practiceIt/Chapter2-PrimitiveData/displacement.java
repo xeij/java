e4d03e815ea71c92634a54a4171119edcf07ad6b
xeij <q08d@aol.com>
2024-06-20 12:17:40 -0700

    6/20/2024

---
+++ src/practiceIt/Chapter2-PrimitiveData/displacement.java
@@ -0,0 +1,6 @@
+double s0 = 12.0;
+double v0 = 3.5;
+double a = 9.8;
+double t = 10.0;
+
+System.out.println( s0 + v0 * t + a * t * t * .5);
---
+++ src/practiceIt/Chapter2-PrimitiveData/loopSquares.java
@@ -0,0 +1,7 @@
+int currentSquare = 1;
+int difference = 3;
+    for (int i = 0; i < 10; i++) {
+        System.out.print(currentSquare + " ");
+        currentSquare += difference;
+        difference += 2;
+}
