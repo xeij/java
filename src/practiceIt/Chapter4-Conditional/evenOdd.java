2e80b494dc87b05f5cafadf4dd13ce8a350f6a60
xeij <q08d@aol.com>
2024-06-24 15:56:43 -0700

    6/24/2024

---
+++ src/practiceIt/Chapter4-Conditional/evenOdd.java
@@ -0,0 +1,9 @@
+public static void method(Scanner console){
+	System.out.print("Type a number: ");
+	int input = console.nextInt();
+	if(input % 2 = 0){
+	System.out.println("even");
+	} else {
+	System.out.println("odd");
+	}
+}
