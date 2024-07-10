827bb84557014948cdb7632418df5263e05c6cf4
xeij <q08d@aol.com>
2024-07-09 18:25:26 -0700

    7/9/2024

---
+++ src/practiceIt/Chapter5-Program Logic/BJP5 Self-Check doWhileSeashore.java
@@ -0,0 +1,8 @@
+Scanner scanner = new Scanner(System.in);
+String input;
+
+do{
+System.out.println("She sells seashells by the seashore.");
+System.out.print("Do you want to hear it again? ");
+input = scanner.next();    
+} while(input.equals("y"));
