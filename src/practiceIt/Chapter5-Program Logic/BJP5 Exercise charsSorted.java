0c80194f1e8004a95ab2741cd0f6e85098777d67
xeij <q08d@aol.com>
2024-07-23 12:21:11 -0700

    7/22/2024

---
+++ src/practiceIt/Chapter5-Program Logic/BJP5 Exercise charsSorted.java
@@ -0,0 +1,8 @@
+public static boolean charsSorted(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) > str.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
