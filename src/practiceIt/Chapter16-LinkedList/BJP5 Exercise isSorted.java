81fcc27363e6aa21cd9362463ec62f44ed8b00d7
xeij <q08d@aol.com>
2024-08-10 18:26:51 -0700

    8/10/2024

---
+++ src/practiceIt/Chapter16-LinkedList/BJP5 Exercise isSorted.java
@@ -0,0 +1,17 @@
+public boolean isSorted() {
+    ListNode pre = front;
+    
+    if (front == null) {
+        return true;
+    }
+    
+    while (pre.next != null) {
+        ListNode current = pre.next;
+        if (current.data < pre.data) {
+            return false;
+        }
+        pre = current;
+    }
+    
+    return true;
+}
