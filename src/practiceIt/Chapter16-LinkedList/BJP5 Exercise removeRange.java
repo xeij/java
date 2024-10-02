7fcf33ecbb04db2e3496916f5d46b038b4c84389
xeij <q08d@aol.com>
2024-10-02 08:44:56 -0700

    10/2/2024

---
+++ src/practiceIt/Chapter16-LinkedList/BJP5 Exercise removeRange.java
@@ -0,0 +1,29 @@
+public void removeRange(int startIndex, int endIndex) {
+    if (startIndex < 0 || endIndex < 0) {
+        throw new IllegalArgumentException();
+    } else {
+        int index = 0;
+        ListNode current = front;
+        ListNode start = front;
+        ListNode end = null;
+        
+        while (current != null) {
+        
+            if (index + 1 == startIndex) {
+                start = current;
+            } else if (index == endIndex) {
+                end = current;
+            }
+            
+            current = current.next;
+            index++;
+        }
+        
+        if (startIndex == 0) {
+            front = end.next;
+        } else {
+            start.next = end.next;
+        }
+        
+    }
+}
