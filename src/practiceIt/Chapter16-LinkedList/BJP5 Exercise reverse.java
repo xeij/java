eefda4208b1eba91ee067028c3cc87ae3555a7d0
xeij <q08d@aol.com>
2024-10-14 13:40:07 -0700

    10/14/2024

---
+++ src/practiceIt/Chapter16-LinkedList/BJP5 Exercise reverse.java
@@ -0,0 +1,19 @@
+public void reverse() {
+    LinkedIntList list = new LinkedIntList();
+    
+    if (front != null) {
+        ListNode current = front.next;
+        front.next = null;
+        list.front = front;
+        
+        while (current != null) {
+            ListNode temp = current;
+            current = current.next;
+            temp.next = list.front;
+            list.front = temp;
+        }
+        
+    }
+    
+    front = list.front;
+}
