57c59b6e55b7021da5d3ba719271277e68a712d8
xeij <q08d@aol.com>
2024-10-28 20:31:08 -0700

    10/28/2024

---
+++ src/practiceIt/Chapter14-StackQueue/BJP5 Exercise removeMin.java
@@ -0,0 +1,31 @@
+public static int removeMin(Stack<Integer> s) {
+    Queue<Integer> q = new LinkedList<Integer>();
+    int min = s.peek();
+    
+    while (!s.isEmpty()) {
+        int num = s.pop();
+        q.add(num);
+        
+        if (min > num) {
+            min = num;
+        }
+    }
+    
+    while (!q.isEmpty()) {
+        int num = q.remove();
+        
+        if (num != min) {
+            s.push(num);
+        }
+    }
+    
+    while (!s.isEmpty()) {
+        q.add(s.pop());
+    }
+    
+    while (!q.isEmpty()) {
+        s.push(q.remove());
+    }
+    
+    return min;
+}
